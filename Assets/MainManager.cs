90850256662b8a623e29d0467b02983edc1288ef
DESKTOP-KF73BIJ\IKO <erke.zhang@yunbu.me>
2020-08-12 23:00:04 +0800

    refresh

---
+++ Assets/MainManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class MainManager : MonoBehaviourPunCallbacks
+{
+    public GameObject readyUI;
+    public void Start()
+    {
+        readyUI.SetActive(true);
+    }
+    public void ReadyToPlay()
+    {
+        readyUI.SetActive(false);
+        PhotonNetwork.Instantiate("Player",new Vector3(-7,-4,0),Quaternion.identity,0);
+    }
+}
---
+++ Assets/RoomNameListManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class RoomNameListManager : MonoBehaviourPunCallbacks
+{
+    public GameObject roomNamePrefab;
+    public Transform gridLayout;
+    public void Start()
+    {
+        
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        //删除
+        for (int i = 0; i < gridLayout.childCount; i++)
+        {
+            if (gridLayout.GetChild(i).gameObject.GetComponentInChildren<Text>().text==roomList[i].Name)
+            {
+                Destroy(gridLayout.GetChild(i).gameObject);
+                if (roomList[i].PlayerCount==0)
+                {
+                    roomList.Remove(roomList[i]);
+                }
+            }
+
+        }
+       // 刷新
+        foreach (var room in roomList)
+        {
+            GameObject newRoom = Instantiate(roomNamePrefab,gridLayout.position,Quaternion.identity);
+            newRoom.GetComponentInChildren<Text>().text = room.Name+"("+room.PlayerCount+")";
+            newRoom.transform.SetParent(gridLayout);
+        }
+    }
+
+}
